b82b4e203c160072194707037af0f73987d3425f
Luiz Rodrigues <lrodrigues5150@gmail.com>
2014-05-26 20:44:42 -0700

    Ignorar Testes Automatizados

--- bdic3/bdic3-webapp/src/test/java/br/ita/bdic3/testes/us08/Haversine.java
+++ bdic3/bdic3-webapp/src/test/java/br/ita/bdic3/testes/us08/Haversine.java
@@ -1,19 +1,24 @@
 package br.ita.bdic3.testes.us08;
 
 public class Haversine {
-public static void main(String[] args) {
-	System.out.println(distanciaEntreCoordenadasEmKm(-18.769, -51.348, -5.256, -44.056	));
-}
-	public static Double distanciaEntreCoordenadasEmKm(Double lat1,
-			Double long1, Double lat2, Double long2) {
-		Integer R = 6371; // raio da terra em km
+
+	private final static Integer R = 6371; // raio da terra em km
+	
+	public static Double distanciaEntreCoordenadasEmKm(
+			Double lat1, Double long1, 
+			Double lat2, Double long2) {
+		
 
 		Double latDistance = toRad(lat2 - lat1);
 		Double lonDistance = toRad(long2 - long1);
 
-		Double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-				+ Math.cos(toRad(lat1)) * Math.cos(toRad(lat2))
-				* Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+		Double a = 
+				Math.sin(latDistance / 2) * 
+				Math.sin(latDistance / 2) + 
+				Math.cos(toRad(lat1)) * 
+				Math.cos(toRad(lat2)) * 
+				Math.sin(lonDistance / 2) * 
+				Math.sin(lonDistance / 2);
 
 		Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
