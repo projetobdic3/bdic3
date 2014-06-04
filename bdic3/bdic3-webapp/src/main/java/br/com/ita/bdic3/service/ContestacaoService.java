93691ec62de7bb25dbbbc957c07c29c3f4069f59
Luiz Rodrigues <ralrodrigues@outlook.com>
2014-06-03 17:31:35 -0700

    Sprint 3 - US 63 - Atualizar ContestacaoService

--- bdic3/bdic3-webapp/src/main/java/br/com/ita/bdic3/service/ContestacaoService.java
+++ bdic3/bdic3-webapp/src/main/java/br/com/ita/bdic3/service/ContestacaoService.java
@@ -1,14 +1,25 @@
 package br.com.ita.bdic3.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import br.com.ita.bdic3.dao.ClienteDao;
+import br.com.ita.bdic3.entity.Cliente;
 import br.com.ita.bdic3.vo.ContestacaoVO;
 
 @Component
 public class ContestacaoService {
+	
+	@Autowired
+	private ClienteDao clienteDao;
 
 	public boolean validarCliente(ContestacaoVO contestacaoVO) {
-		return true;
+		Cliente cliente = clienteDao.findByNomeAndCpf(contestacaoVO.getNomeCliente(), contestacaoVO.getCpfCliente());
+		if(cliente != null){
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
