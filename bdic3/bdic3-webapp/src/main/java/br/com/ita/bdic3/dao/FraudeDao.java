045d70c7f8a38cd8d6707b04fed993484f8f169b
Luiz Rodrigues <ralrodrigues@outlook.com>
2014-06-11 15:04:25 -0700

    SP3 - US63 - Criar FraudeDao

---
+++ bdic3/bdic3-webapp/src/main/java/br/com/ita/bdic3/dao/FraudeDao.java
@@ -0,0 +1,14 @@
+package br.com.ita.bdic3.dao;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.ita.bdic3.entity.Fraude;
+
+@Repository("fraudeDao")
+public class FraudeDao extends GenericDao<Long, Fraude> {
+
+	public FraudeDao() {
+		super(Fraude.class);
+	}
+
+}
